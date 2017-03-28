9462a17419ff136937de362486eb92c5509fe80b
Angelo Gargantini <angelo.gargantini@unibg.it>
2017-03-28 14:36:03 +0200

    alura corretto

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package helloWorld;
 
 public class HelloWorld {
 
-	private static final String HELLO_WORLD = "Hello World";
+	private static final String HELLO_WORLD = "Alura World";
 
 	public String sayHello() {
 		return HELLO_WORLD;
