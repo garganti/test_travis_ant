5286a331f25eba779d91f79effd33fa6893d4cf0
Angelo Gargantini <angelo.gargantini@unibg.it>
2019-03-13 09:49:10 +0100

    modifiche

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package helloWorld;
 
 public class HelloWorld {
 
-	private static final String HELLO_WORLD = "Hello World kkk";
+	private static final String HELLO_WORLD = "Hello World";
 
 	public String sayHello() {
 		return HELLO_WORLD;
