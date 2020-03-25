d4986f2d5db094b97002c78979fb26d7a566dbf1
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-03-25 09:51:50 +0100

    primo test

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package helloWorld;
 
 public class HelloWorld {
 
-	private static final String HELLO_WORLD = "CIao come stai?";
+	private static final String HELLO_WORLD = "Hello World";
 
 	public String sayHello() {
 		return HELLO_WORLD;
